8399fb41531157e9ecff30cff24548c169b10000
Ethan Brandon Bates <batese14@gmail.com>
2018-07-29 01:31:39 +0000

    added order models and users app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categories, Menu, Prices
+
+admin.site.register(Categories)
+admin.site.register(Menu)
+admin.site.register(Prices)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+class Menu(models.Model):
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="category")
+    menu_item = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.menu_item
+
+
+class Prices(models.Model):
+    ITEM_SIZES = (
+        ('S', 'Small'),
+        ('L', 'Large'),
+    )
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    item_size = models.CharField(max_length=1, choices=ITEM_SIZES)
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.item_size} {self.item_price}"
