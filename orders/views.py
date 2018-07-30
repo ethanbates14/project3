76798a234605c9e8482284742d8d08997c824c9c
Ethan Brandon Bates <batese14@gmail.com>
2018-07-30 00:58:29 +0000

    modifications to orders data

--- orders/views.py
+++ orders/views.py
@@ -6,8 +6,21 @@ from .models import Categories, Menu, Prices
 
 # Orders Views
 def index(request):
+
+    #build data array
+    pin_category = Categories.objects.all()
+    pin_foods = Menu.objects.all()
+    food_items = []
+
+    for item in pin_category:
+        food_items.append(item.category_name)
+        query_item = pin_foods.filter(category_id = item.id)
+        print(query_item)
+        print(food_items)
+
+
     context = {
-        "food_cat": Categories.objects.all(),
+        "food_categories": Categories.objects.all(),
         "menu": Menu.objects.all()
     }
     return render(request, "orders/index.html", context)
